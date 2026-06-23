030b78699ee4ff192c4b53d4abc6c5d6ef1f04c9
ayush <ayush.an004@gmail.com>
2026-06-23 15:08:25 +0530

    password

--- Python/Day2/index.py
+++ Python/Day2/index.py
@@ -31,18 +31,32 @@
 
 
 #zomato coupon
-from datetime import datetime
-name=str(input("Enter your name"))
-birthday=str(input("Enter birthday in format yyyy-mm-dd"))
-date_object = datetime.strptime(birthday, "%Y-%m-%d")
-birthday_month=date_object.month
-number_of_order=10
-total_spent=1110
-if date_object.month==date_object.month and total_spent>1000 and number_of_order>5:
-    print("highest order discount with birthday discount")
-elif birthday_month==date_object.month:
-    print("Birthday month discount!!!!")
-elif number_of_order>5:
-    print("returning customer discount")
-elif total_spent>1000:
-    print("high value customer discount")
+# from datetime import datetime
+# name=str(input("Enter your name"))
+# birthday=str(input("Enter birthday in format yyyy-mm-dd"))
+# date_object = datetime.strptime(birthday, "%Y-%m-%d")
+# birthday_month=date_object.month
+# number_of_order=10
+# total_spent=1110
+# if date_object.month==date_object.month and total_spent>1000 and number_of_order>5:
+#     print("highest order discount with birthday discount")
+# elif birthday_month==date_object.month:
+#     print("Birthday month discount!!!!")
+# elif number_of_order>5:
+#     print("returning customer discount")
+# elif total_spent>1000:
+#     print("high value customer discount")
+
+
+
+user_name = input("Enter your username: ")
+password = input("Enter your password: ")
+saved_password = "122345"
+saved_username = "yesss"
+
+if user_name == saved_username and password == saved_password:
+    print("Welcome " + user_name)
+else:
+    print("Wrong name/password")
+
+
