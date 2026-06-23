ce7b1a5c0fdf3e78a2b85f574db3766b78a49c9e
ayush <ayush.an004@gmail.com>
2026-06-23 12:20:29 +0530

    if else code

--- Python/Day2/index.py
+++ Python/Day2/index.py
@@ -1,7 +1,18 @@
-a=int(input("Please add age: "))
-if a<18:
+age=int(input("Please add age: "))
+if age<18:
     print("You are young and can not vote")
-elif 18 <= a <= 100:
+elif 18 <= age <= 100:
     print("You can vote")
 else:
     print("Enter valid age")
+
+
+salary = int(input("Please add salary: "))
+if salary>1000000:
+    print("Salary eligible for tier 1 gift")
+elif 700000<salary<=1000000:
+    print("Salary eligible for tier 2 gift")
+elif 300000<salary<=500000:
+    print("Salary eligible for tier 3 gift")
+else:
+    print("No gift")
