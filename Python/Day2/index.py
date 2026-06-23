29929f9041108b74f53f2a6a540567ba43196484
ayush <ayush.an004@gmail.com>
2026-06-23 12:44:58 +0530

    prime number testing

--- Python/Day2/index.py
+++ Python/Day2/index.py
@@ -1,18 +1,28 @@
-age=int(input("Please add age: "))
-if age<18:
-    print("You are young and can not vote")
-elif 18 <= age <= 100:
-    print("You can vote")
-else:
-    print("Enter valid age")
+# age=int(input("Please add age: "))
+# if age<18:
+#     print("You are young and can not vote")
+# elif 18 <= age <= 100:
+#     print("You can vote")
+# else:
+#     print("Enter valid age")
+#
+#
+# salary = int(input("Please add salary: "))
+# if salary>1000000:
+#     print("Salary eligible for tier 1 gift")
+# elif 700000<salary<=1000000:
+#     print("Salary eligible for tier 2 gift")
+# elif 300000<salary<=500000:
+#     print("Salary eligible for tier 3 gift")
+# else:
+#     print("No gift")
 
+#using function
+def isprime(number):
+    for i in range(2, number):
+        if number % i == 0:
+            return False
 
-salary = int(input("Please add salary: "))
-if salary>1000000:
-    print("Salary eligible for tier 1 gift")
-elif 700000<salary<=1000000:
-    print("Salary eligible for tier 2 gift")
-elif 300000<salary<=500000:
-    print("Salary eligible for tier 3 gift")
-else:
-    print("No gift")
+    return True
+number=int(input("Enter number to check"))
+print(isprime(number))
