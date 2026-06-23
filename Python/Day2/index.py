19ec8f7321ffd9c72aaa18e83f73a34125aa6fd4
ayush <ayush.an004@gmail.com>
2026-06-23 14:36:55 +0530

    coupon check

--- Python/Day2/index.py
+++ Python/Day2/index.py
@@ -18,11 +18,31 @@
 #     print("No gift")
 
 #using function
-def isprime(number):
-    for i in range(2, number):
-        if number % i == 0:
-            return False
+# def isprime(n):
+#     for i in range(2, n):
+#         if n % i == 0:
+#             return False
+#
+#     return True
+# number=int(input("Enter number to check"))
+# print(isprime(number))
 
-    return True
-number=int(input("Enter number to check"))
-print(isprime(number))
+
+
+
+#zomato coupon
+from datetime import datetime
+name=str(input("Enter your name"))
+birthday=str(input("Enter birthday in format yyyy-mm-dd"))
+date_object = datetime.strptime(birthday, "%Y-%m-%d")
+birthday_month=date_object.month
+number_of_order=10
+total_spent=1110
+if date_object.month==date_object.month and total_spent>1000 and number_of_order>5:
+    print("highest order discount with birthday discount")
+elif birthday_month==date_object.month:
+    print("Birthday month discount!!!!")
+elif number_of_order>5:
+    print("returning customer discount")
+elif total_spent>1000:
+    print("high value customer discount")
