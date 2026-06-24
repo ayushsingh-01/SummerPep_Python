49be2c8ee9446ab1e69deedf58b8df71ad14d7f7
ayush <ayush.an004@gmail.com>
2026-06-24 14:49:46 +0530

    prime number

--- Python/Day3/index.py
+++ Python/Day3/index.py
@@ -29,27 +29,80 @@
 # print(sum)
 
 
-#factorial
-fact=1
-for i in range(1,11):
-    fact*=i
-print(fact)
+# #factorial
+# fact=1
+# for i in range(1,11):
+#     fact*=i
+# print(fact)
+#
+#
+# #count digit
+# number=9876
+# s=str(number)
+# c=0
+# for i in s:
+#     c+=1
+# print(c)
+#
+#
+# #sum of digits
+# n=1234
+# suu=0
+# for i in str(n):
+#     suu+=int(i)
+# print(suu)
 
 
-#count digit
-number=9876
-s=str(number)
+
+
+#palindrome number
+a=int(input())
+c=a
+b=0
+while a>0:
+    r=a%10
+    b=(b*10)+r
+    a=a//10
+if c==b:
+    print("YES")
+else:
+    print("NO")
+
+
+
+
+#reverse number
+num=1234
+r_num=0
+while num!=0:
+    d=num%10
+    r_num=(r_num*10)+d
+    num//=10
+print(r_num)
+
+
+#fibonacci
+len=10
+a,b=0,1
 c=0
-for i in s:
+while c<len:
+    print(a," ")
+    a,b=b,a+b
     c+=1
-print(c)
 
-
-#sum of digits
-n=1234
-suu=0
-
-for i in str(n):
-    suu+=int(i)
-print(suu)
-
+#prime number
+num=int(input())
+if num<=1:
+ print("not prime")
+else:
+ divisor=2
+ is_prime=True
+ while divisor*divisor<=num:
+  if num%divisor==0:
+   is_prime=False
+   break
+  divisor+=1
+if is_prime:
+  print("is prime")
+else:
+  print("not prime")
