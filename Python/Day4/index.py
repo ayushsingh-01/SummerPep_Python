41a1a5dfdfe7206a8e637e5e8b44fb64d85a0c31
ayush <ayush.an004@gmail.com>
2026-06-25 22:21:51 +0530

    functions

--- Python/Day4/index.py
+++ Python/Day4/index.py
@@ -1,30 +1,50 @@
-#question 1 nested loop
-for i in range(0,3):
-    for j in range(0,3):
-        print(i,"",j)
+# #question 1 nested loop
+# for i in range(0,3):
+#     for j in range(0,3):
+#         print(i,"",j)
+#
+#
+# #question 2
+# for i in range(1,4):
+#     print("row",i)
+#     for j in range(1,4):
+#         print("column",j)
+#
+#
+# #question 3
+# for i in range(3):
+#     for j in range(4):
+#         print("*", end=" ")
+#     print()
+#
+#
+# #question 4
+# for i in range(3):
+#     for j in range(4):
+#         print(chr(j+65),end="")
+#     print()
+#
+#
+# # #question 9
+# # for i in range(3):
+# #     for j in range(4):
+#
+#
+# for i in range(1,5):
+#     for j in range(4):
+#         print(i,end=" ")
+#     print()
+#
 
 
-#question 2
-for i in range(1,4):
-    print("row",i)
-    for j in range(1,4):
-        print("column",j)
 
 
-#question 3
-for i in range(3):
-    for j in range(4):
-        print("*", end=" ")
-    print()
+
+#
+# def add(x,y):
+#     print(a+b)
+# a=10
+# b=20
+# add(a,b)
 
 
-#question 4
-for i in range(3):
-    for j in range(4):
-        print(chr(j+65),end="")
-    print()
-
-
-#question 9
-for i in range(3):
-    for j in range(4):
