e4f5e425e2a3c6e8f9401b0506f8a02f6afd16f7
ayush <ayush.an004@gmail.com>
2026-06-25 12:09:23 +0530

    practice

--- Python/Day3/index.py
+++ Python/Day3/index.py
@@ -54,6 +54,99 @@
 
 
 
+#
+# #palindrome number
+# a=int(input())
+# c=a
+# b=0
+# while a>0:
+#     r=a%10
+#     b=(b*10)+r
+#     a=a//10
+# if c==b:
+#     print("YES")
+# else:
+#     print("NO")
+#
+#
+#
+#
+# #reverse number
+# num=1234
+# r_num=0
+# while num!=0:
+#     d=num%10
+#     r_num=(r_num*10)+d
+#     num//=10
+# print(r_num)
+#
+#
+# #fibonacci
+# len=10
+# a,b=0,1
+# c=0
+# while c<len:
+#     print(a," ")
+#     a,b=b,a+b
+#     c+=1
+#
+# #prime number
+# num=int(input())
+# if num<=1:
+#  print("not prime")
+# else:
+#  divisor=2
+#  is_prime=True
+#  while divisor*divisor<=num:
+#   if num%divisor==0:
+#    is_prime=False
+#    break
+#   divisor+=1
+# if is_prime:
+#   print("is prime")
+# else:
+#   print("not prime")
+
+
+
+#even numbe using while loop
+i=1
+while(i<50):
+    if i%2==0:
+        print(i)
+    i+=1
+
+
+#3
+while i in range(5):
+    print(i*"*")
+
+
+# number of digits
+number=int(input())
+s=str(number)
+c=0
+while i in s:
+    c+=1
+print(c)
+
+
+#reverse number
+num=1234
+r_num=0
+while num!=0:
+    d=num%10
+    r_num=(r_num*10)+d
+    num//=10
+print(r_num)
+
+
+#multiplication
+a=int(input())
+while i in range(1,11):
+    print(a," X ",i," = ",i*a)
+
+
 
 #palindrome number
 a=int(input())
@@ -69,40 +162,29 @@ else:
     print("NO")
 
 
+#sum of digits
+n=1234
+suu=0
+while i in str(n):
+    suu+=int(i)
+print(suu)
 
 
-#reverse number
-num=1234
-r_num=0
-while num!=0:
-    d=num%10
-    r_num=(r_num*10)+d
-    num//=10
-print(r_num)
-
-
-#fibonacci
-len=10
-a,b=0,1
+#taking number and adding till user types 0
+b=int(input())
 c=0
-while c<len:
-    print(a," ")
-    a,b=b,a+b
-    c+=1
+while a!=0:
+    c+=a
+    b=int(input())
 
-#prime number
-num=int(input())
-if num<=1:
- print("not prime")
-else:
- divisor=2
- is_prime=True
- while divisor*divisor<=num:
-  if num%divisor==0:
-   is_prime=False
-   break
-  divisor+=1
-if is_prime:
-  print("is prime")
-else:
-  print("not prime")
+
+#skip multiple of 3
+while i in range(1,20):
+    if(i%3!=0):
+        print(i)
+
+#skip multiple of 4
+j=1
+while j<20:
+    if(j%4!=0):
+        print(j)
---
+++ Python/Day4/index.py
@@ -0,0 +1,30 @@
+#question 1 nested loop
+for i in range(0,3):
+    for j in range(0,3):
+        print(i,"",j)
+
+
+#question 2
+for i in range(1,4):
+    print("row",i)
+    for j in range(1,4):
+        print("column",j)
+
+
+#question 3
+for i in range(3):
+    for j in range(4):
+        print("*", end=" ")
+    print()
+
+
+#question 4
+for i in range(3):
+    for j in range(4):
+        print(chr(j+65),end="")
+    print()
+
+
+#question 9
+for i in range(3):
+    for j in range(4):
