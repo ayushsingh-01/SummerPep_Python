d2eebda5462b7e32ef583f682ed1c5e090b7e74d
ayush <ayush.an004@gmail.com>
2026-06-26 23:35:30 +0530

    day 5 python

---
+++ Python/Day5/index.py
@@ -0,0 +1,157 @@
+# #1
+# for i in range(5):
+#     print("*")
+# print()
+#
+#
+# #2
+# for i in range(3):
+#     for j in range(4):
+#         print("*", end=" ")
+#     print()
+#
+#
+# #3
+# for i in range(5):
+#     print("*"*i)
+# print()
+# #4
+# for i in range(5,0,-1):
+#     print("*"*i)
+# print()
+# #5
+# for i in range(3):
+#     for j in range(4):
+#         print(chr(j+65),end="")
+#     print()
+# #6
+# for i in range(5, 0, -1):
+#     for j in range(1, i + 1):
+#         print(j, end="")
+#     print()
+# #7
+# for i in range(0,5,1):
+#     for j in range(1, i + 1):
+#         print(j, end="")
+#     print()
+#
+# #8
+# for i in range(3):
+#     for j in range(4):
+#         print(i, end=" ")
+#     print()
+# #9
+# for i in range(6):
+#     for j in range(0,i+1):
+#         print(chr(j+65), end=" ")
+#     print()
+# #10
+# for i in range(6,-1,-1):
+#     for j in range(0,i+1):
+#         print(chr(j+65), end=" ")
+#     print()
+#
+# #11
+# for i in range(6):
+#     for j in range(i):
+#         print(i,end=" ")
+#     print()
+# #12
+# for i in range(6,0,-1):
+#     for j in range(i):
+#         print(i,end=" ")
+#     print()
+#
+#
+# for i in range(5):
+#     for j in range(5, i, -1):
+#         print(j, end="")
+#     print()
+
+#
+# for building in range(1,4):
+#     for floor in range(0,8):
+#         for room in range(1,11):
+#             print("building",building,"floor", floor, "room", room)
+
+
+
+
+"""
+Section A
+1. Loop inside a loop inside a loop
+2. One Loop One
+3. Inner
+4. Outer
+5. true
+6. false
+7. State
+8. 3
+9. 5*5*5
+10. folder directory
+Section B
+11. 0 0 0  0 0 1  0 1 0  0 1 1  1 0 0  1 0 1  1 1 0  1 1 1
+12. 3 4 5 6 7 8
+13.
+**
+**
+
+**
+**
+14.
+Layer 0
+Row 0
+0 1 2
+Row 1
+0 1 2
+Layer 1
+Row 0
+0 1 2
+Row 1
+0 1 2
+15. 12
+16.0 0 0
+0 0 2
+0 1 0
+0 1 2
+1 0 0
+1 0 2
+1 1 0
+1 1 2
+17. a) 3
+b) 6
+c) 24
+18.1
+2
+2
+4
+3
+6
+19.room
+building
+12
+2 × 3 × 2 = 12
+20.0 0 0
+0 0 1
+0 1 0
+0 1 1
+1 0 0
+21. 1 1 1
+1 2 1
+2 1 1
+2 2 1
+22.Iteration	count
+1	1
+2	2
+3	3
+4	4
+5	5
+6	6
+7	7
+8	8
+23.
+24.
+25.
+
+
+"""
